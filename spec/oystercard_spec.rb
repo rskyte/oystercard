2e46f29dfbe55b84022d4bd5e18426934bdce139
Rhys Kyte <rskyte@gmail.com>
2017-11-29 13:07:30 +0000

    before test block further extracted and skipped on certain tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,18 +3,24 @@ require 'oystercard.rb'
 describe Oystercard do
   subject(:card) { described_class.new }
   let(:station) { double(:my_station, name: 'name') }
+  before do |example|
+    unless example.metadata[:skip_before]
+      card.topup(20)
+      card.touch_in(station)
+    end
+  end
 
   describe 'Card balance' do
-    it 'should be initialised with a default balance of 0' do
+
+    it 'should be initialised with a default balance of 0', :skip_before do
       expect(card.balance).to eq 0
     end
 
     it 'should be able to be topped-up' do
-      card.topup(50)
-      expect(card.balance).to eq 50
+      expect(card.balance).to eq 20
     end
 
-    it 'should have a maximum balance of 90' do
+    it 'should have a maximum balance of 90', :skip_before do
       maxed_limit = Oystercard::MAXIMUM_BALANCE
       expect { card.topup(maxed_limit + 1) }
         .to raise_error "Maximum Balance Is #{maxed_limit}"
@@ -22,31 +28,23 @@ describe Oystercard do
   end
 
   context 'Card in use' do
-
     describe '#touch_in' do
 
       it 'should touch in' do
-        card.topup(20)
-        card.touch_in(station)
         expect(card.in_journey?).to eq true
       end
 
-      it 'should not let you touch in with balance less than 1' do
-        expect { card.touch_in(station) }.to raise_error 'Insufficient Funds'
+      it 'should not let you touch in with balance less than 1', :skip_before do
+        expect { card.touch_in(station) }
+          .to raise_error 'Insufficient Funds'
       end
 
       it 'remembers touch in station' do
-        card.topup(20)
-        card.touch_in(station)
         expect(card.entry_station).to eq 'name'
       end
     end
 
     describe '#touch_out' do
-      before do
-        card.topup(20)
-        card.touch_in(station)
-      end
 
       it 'should touch out' do
         card.touch_out
